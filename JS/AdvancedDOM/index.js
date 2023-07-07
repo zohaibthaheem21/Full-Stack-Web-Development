692e8a6b9969635175b4a5be4cddc876dd77598f
Zohaib Ali Thaheem <zohaibalisw21@gmail.com>
2023-07-07 18:06:53 +0500

    init

---
+++ JS/AdvancedDOM/index.js
@@ -0,0 +1,7 @@
+var numberOfButtons = document.querySelectorAll(".drum").length;
+
+for (var i=0; i<numberOfButtons; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click",function () {
+        alert("I got clicked");
+    });
+}
