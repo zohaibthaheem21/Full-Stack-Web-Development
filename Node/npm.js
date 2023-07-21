f94dc7028461bba9b22f1f5585b2f854f2c1d793
Zohaib Ali Thaheem <zohaibalisw21@gmail.com>
2023-07-21 18:11:09 +0500

    init

---
+++ Node/npm.js
@@ -0,0 +1,18 @@
+// Generation random name:
+
+// var generateName = require('sillyname');
+
+// import generateName from "sillyname";
+// var sillyName = generateName();
+
+// console.log(`Hello My name is ${sillyName}.`);
+
+
+
+// Generating random superhero name: 
+ 
+const superheroes = require('superheroes');
+ 
+const name = superheroes.random();
+
+console.log(`I am ${name}.`);
