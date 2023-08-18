8f5e266059c64ea188787bb48fef04a0e51d2918
Zohaib Ali Thaheem <zohaibalisw21@gmail.com>
2023-08-18 22:54:34 +0500

    init

---
+++ JS/JavaScript/string.js
@@ -0,0 +1,21 @@
+let na = "zohaib";
+console.log(na);
+console.log(na.length);
+
+let a = "Ahmed";
+let b = "Aslam";
+
+console.log(`${a} is the best friend of ${b}`);
+
+// Escape Character:
+
+console.log("Hello this is me \'Zohaib\'");
+console.log("Hello this is me \nZohaib");
+console.log("Hello this is me \tZohaib");
+
+
+// Quick Quiz:
+let fr = "Zohaib";
+for (let i=0; i<fr.length; i++) {
+    console.log(fr[i]);
+}
