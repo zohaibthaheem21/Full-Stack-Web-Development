dfde75233ba154134997ac5eb7a2a82e01488479
Zohaib Ali Thaheem <zohaibalisw21@gmail.com>
2023-09-16 15:03:26 +0500

    init

---
+++ JS/JavaScript/D.js
@@ -0,0 +1,17 @@
+// console.dir
+
+console.dir(document.getElementsByTagName('span')[0]);
+span
+
+// console.log
+
+console.log(document.getElementsByTagName('span')[0]);
+<span>​Hey I am span​</span>
+
+
+// document.body.firstChild
+// " Hello World "
+// document.body.firstChild.data
+// '\n    Hello World\n    '
+// document.body.firstChild.nodeValue
+// '\n    Hello World\n
