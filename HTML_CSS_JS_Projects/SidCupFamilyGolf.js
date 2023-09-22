3f1c866220bcf9c0396a0024cf39c7b1c154b606
Zohaib Ali Thaheem <zohaibalisw21@gmail.com>
2023-09-22 17:55:29 +0500

    init

--- HTML_CSS_JS_Projects/SidCupFamilyGolf.js
+++ HTML_CSS_JS_Projects/SidCupFamilyGolf.js
@@ -1,3 +1,15 @@
+var crsr = document.querySelector("#cursor");
+var crsr_b = document.querySelector("#cursor-b");
+
+document.addEventListener("mousemove",function(dets) {
+    crsr.style.left = dets.x+"px"
+    crsr.style.top = dets.y+"px"
+    crsr_b.style.left = dets.x - 150 +"px"
+    crsr_b.style.top = dets.y - 150 +"px"
+})
+
+
+
 gsap.to("#nav",{
     backgroundColor: "#000",
     height: "100px",
@@ -5,10 +17,21 @@ gsap.to("#nav",{
     scrollTrigger:{
         trigger:"#nav",
         scroller:"body",
-        markers:true,
+        // markers:true,
         start:"top -10%",
         end:"top -11%",
         scrub:1
     }
+})
 
+gsap.to("#main",{
+    backgroundColor:"#000",
+    scrollTrigger:{
+        trigger:"#main",
+        scroller:"body",
+        // markers:true,
+        start:"top -25%",
+        end:"top -70%",
+        scrub:2
+    }
 })
