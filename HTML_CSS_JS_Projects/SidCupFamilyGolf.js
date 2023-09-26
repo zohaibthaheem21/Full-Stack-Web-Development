46af7f5c05bf9063f007e91862d7d248c7214a0c
Zohaib Ali Thaheem <zohaibalisw21@gmail.com>
2023-09-26 17:24:45 +0500

    init

--- HTML_CSS_JS_Projects/SidCupFamilyGolf.js
+++ HTML_CSS_JS_Projects/SidCupFamilyGolf.js
@@ -35,3 +35,81 @@ gsap.to("#main",{
         scrub:2
     }
 })
+
+var h4All = document.querySelectorAll("#nav h4")
+
+h4All.forEach(function(e){
+    e.addEventListener("mouseenter",function(){
+        crsr.style.scale = 3
+        crsr.style.border = "1px solid white"
+        crsr.style.backgroundColor = "transparent"
+    })
+
+    e.addEventListener("mouseleave",function(){
+        crsr.style.scale = 1
+        crsr.style.border = "1px solid #95C11E"
+        crsr.style.backgroundColor = "#95C11E"
+    })
+})
+
+gsap.from("#about-us img, #aboutUs",{
+    y:90,
+    opacity:0,
+    duration:1,
+    scrollTrigger:{
+        trigger:"#about-us",
+        scroll:"body",
+        start:"top 70%",
+        end:"top 65%",
+        scrub:1
+    }
+})
+
+// gsap.from(".cards",{
+//     scale:0.2,
+//     opacity:0,
+//     duration:1,
+//     stagger:0.1,
+//     scrollTrigger:{
+//         trigger:".cards",
+//         scroll:"body",
+//         start:"top 70%",
+//         end:"top 65%",
+//         scrub:1
+//     }
+// })
+
+gsap.from("#colon1",{
+    y:-70,
+    x:-70,
+    scrollTrigger:{
+        trigger:"#colon1",
+        scroll:"body",
+        start:"top 55%",
+        end:"top 45%",
+        scrub:5
+    }
+})
+
+gsap.from("#colon2",{
+    y:70,
+    x:70,
+    scrollTrigger:{
+        trigger:"#colon1",
+        scroll:"body",
+        start:"top 55%",
+        end:"top 45%",
+        scrub:5
+    }
+})
+
+gsap.from("#page4 h1",{
+    y:50,
+    scrollTrigger:{
+        trigger:"#page4 h1",
+        scroll:"body",
+        start:"top 85%",
+        end:"top 85%",
+        scrub:3
+    }
+})
