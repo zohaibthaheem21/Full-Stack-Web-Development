67adc7a3c315378ada68ff9d208f443a2bf0bbf1
Zohaib Ali Thaheem <zohaibalisw21@gmail.com>
2023-10-13 22:51:58 +0500

    init

---
+++ My Portfolio/script.js
@@ -0,0 +1,8 @@
+// script for animation text of home page
+var typed = new Typed(".auto-type",{
+    strings : ["Software Engineer", "Programmer", "Web Developer","Freelancer"],
+    typeSpeed : 50,
+    backSpeed : 50,
+    loop : true,
+    backDelay: 1000
+})
