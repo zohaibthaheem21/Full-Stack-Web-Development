e74d3ac4c3871e0c2b270a0775af4c1ed523189e
Zohaib Ali Thaheem <zohaibalisw21@gmail.com>
2023-10-14 13:41:12 +0500

    init

--- My Portfolio/script.js
+++ My Portfolio/script.js
@@ -1,8 +1,15 @@
-// script for animation text of home page
-var typed = new Typed(".auto-type",{
-    strings : ["Software Engineer", "Programmer", "Web Developer","Freelancer"],
-    typeSpeed : 50,
-    backSpeed : 50,
-    loop : true,
-    backDelay: 1000
-})
+// using typed.js library for animation text of home page
+// var typed = new Typed(".auto-type",{
+//     strings : ["Software Engineer", "Programmer", "Web Developer","Freelancer"],
+//     typeSpeed : 50,
+//     backSpeed : 50,
+//     loop : true,
+//     backDelay: 1000
+// });
+
+
+hamburger = document.querySelector(".hamburger");
+hamburger.onclick = function() {
+    navBar =  document.querySelector(".nav-bar");
+    navBar.classList.toggle(".active");
+}
